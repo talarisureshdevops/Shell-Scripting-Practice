19aae5a2da185ae34854eb7ea2079a9f19d88a99
talarisureshdevops <TalariSuresh.career@email.com>
2026-02-26 16:04:44 +0530

    all arguments

--- 09-hello-world.sh
+++ 09-hello-world.sh
@@ -1,12 +1,12 @@
 #!/bin/bash/
 
-all variables passed to the script:$@
-number of variables passed to script:$H
-script name :$0
-present which directory you are in : $pwd
-who is running the script :$user
-home of the directory the:$home
-pid of the script:$$
+echo "all variables passed to the script:$@"
+echo "number of variables passed to script:$#"
+echo "script name :$0"
+echo "present which directory you are in : $(pwd)"
+echo "who is running the script :$USER"
+echo "home of the directory the:$HOME"
+echo "pid of the script:$$"
 sleep 100 &
-background process id:$!
-all args passed to script:$*.
+echo "background process id:$!"
+echo "all args passed to script:$*."
