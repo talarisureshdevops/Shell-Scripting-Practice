263d0e115866154907297017956b798b8cab6306
talarisureshdevops <TalariSuresh.career@email.com>
2026-02-26 15:03:09 +0530

    add location file

---
+++ 02-hello-world.sh
@@ -0,0 +1,7 @@
+#!/bin/bash/
+
+echo "suresh:: hello ramesh, how are you?"
+echo "ramesh:: hi suresh, I am fine thanks for asking, how are you doing?"
+echo "suresh:: doing fine, what are you learning ramesh?"
+echo "ramesh:: I am learning shell, what about you?"
+echo "suresh:: I am plaininig to learn Devops"
