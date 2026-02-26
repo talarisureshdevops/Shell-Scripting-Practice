0aa2a01f1fd0c1ff7be092a32031692ac4e45cd5
talarisureshdevops <TalariSuresh.career@email.com>
2026-02-26 15:20:48 +0530

    just varabiles checking

---
+++ 04-hello-world.sh
@@ -0,0 +1,10 @@
+#!/bin/bash/
+
+person1=$1
+person2=$2
+
+echo "$person1:: hello $person2, how are you?"
+echo "$person2:: hi $person1, I am fine thanks for asking, how are you doing?"
+echo "$person1:: doing fine, what are you learning $person2?"
+echo "$person2:: I am learning shell, what about you?"
+echo "$person1:: I am plaininig to learn Devops"
