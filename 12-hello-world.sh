783bc12241f3ddee73471eea2d4b74ea6eda0170
talarisureshdevops <TalariSuresh.career@email.com>
2026-02-26 17:22:33 +0530

    added

--- 12-hello-world.sh
+++ 12-hello-world.sh
@@ -1,8 +1,12 @@
 #!/bin/bash/
-NUMBER=19
+NUMBER=21
 
 if [ $NUMBER -gt 20 ]; then 
 echo "given number is :$NUMBER is greater than 20"
+elif [ $NUMBER -eq 21 ]; then
+echo "given number is :$NUMBER is equl to 21"
+else 
+echo "this number is not valid: $NUMBER"
 
 
 fi
