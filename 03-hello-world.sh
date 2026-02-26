7db7ed9f46ba8096f59df0c39192e4222941aa75
talarisureshdevops <TalariSuresh.career@email.com>
2026-02-26 15:09:30 +0530

    hello variables

---
+++ 03-hello-world.sh
@@ -0,0 +1,9 @@
+#!/bin/bash/
+person1=suresh
+person2=ramesh
+
+echo "$person1:: hello $person2, how are you?"
+echo "$person2:: hi $person1, I am fine thanks for asking, how are you doing?"
+echo "$person1:: doing fine, what are you learning $person2?"
+echo "$person2:: I am learning shell, what about you?"
+echo "$person1:: I am plaininig to learn Devops"
