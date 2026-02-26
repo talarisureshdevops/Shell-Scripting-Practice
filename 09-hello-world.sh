f96b4d244b46af2e0c751665d573714edc3ebcec
talarisureshdevops <TalariSuresh.career@email.com>
2026-02-26 15:58:27 +0530

    just passed through variables

---
+++ 09-hello-world.sh
@@ -0,0 +1,12 @@
+#!/bin/bash/
+
+all variables passed to the script:$@
+number of variables passed to script:$H
+script name :$0
+present which directory you are in : $pwd
+who is running the script :$user
+home of the directory the:$home
+pid of the script:$$
+sleep 100 &
+background process id:$!
+all args passed to script:$*.
